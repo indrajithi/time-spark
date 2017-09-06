67e83cfedab3a1c05775ee44a1bdcd17ed042cec
l1f3h4ck <i.indrajith.143@gmail.com>
2017-09-06 15:17:40 +0530

    percentage sale

--- stat.py
+++ stat.py
@@ -12,11 +12,33 @@ file_dir    = "chicago"
 file_path   = os.path.join(".",file_dir)
 files       = glob.glob(file_path + "/*")
 files.sort()
-
+cwd = os.getcwd()
 filelds = ['PartNumber','QuantityAvailable']
 
-first_flag = True
 
+def Sale(a):
+     sale = 0
+     for i in range(len(a) -1 ):
+         if a[i+1] < a[i]:
+             sale += a[i] - a[i + 1 ]
+     return sale
+
+def Replenish(a):
+     repl = 0
+     for i in range(len(a) -1 ):
+         if a[i+1] > a[i]:
+             repl += a[i + 1] - a[i]
+     return repl
+
+def Analysis(a):
+    sale , repl = Sale(a), Replenish(a)
+    if (a[0] + repl ) != 0:
+        percentage_sale = (float(sale) / (a[0] + repl )) *100 
+    else: percentage_sale = 0
+
+    return sale, repl, round(percentage_sale, 2)
+
+first_flag = True
 for file_no in range(7,14):
     
     #load csv in pandas and create dataframe
@@ -40,7 +62,25 @@ for file_no in range(7,14):
         first_flag = False
 
 
-stdev = df_acc.map(lambda (x, y): (x, st.stdev(y)))
+res = df_acc.map(lambda (x, y): (x, Analysis(y))) \
+.map(lambda (x,(a,b,c)): (x,[a]+[b]+[c]))
+
+#res = df_acc.map(lambda (x, y): (x, round(st.stdev(y),2), Analysis(y))) \
+#.map(lambda (x,y,(a,b,c)): (x,[y]+[a]+[b]+[c]))
+
+
+#sorted by maximum sale
+maximum_sold = res.sortBy(lambda (x,y): y[0] , ascending=False)
+
+#write = maximum_sold.map(lambda (x,y): [x]+y)
+#filter products with atleast one sale and map from (k,(v1,v2,v3)) to [k,v1,v2,v3] 
+write = maximum_sold.filter(lambda (x,y): y[0]>=1).map(lambda (x,y): [x] +y)
+
+#Convert RDD to DataFrame
+df = sqlContext.createDataFrame(write, ['PartNumber', 'Sale','Replenish','PercentageSale'])
+
+#save dataframe as csv
+df.coalesce(1).write.format('com.databricks.spark.csv').options(header='true').save(cwd+'cv_out')
 
 #all the procucts that have standard deviation of 1
 stdOf1 = stdev.filter(lambda (x,y): y>1.0)
